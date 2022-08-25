bcec9c510ad9c10aa3284ad4053626a859ea3960
Ghost_cat174 <93802888+KittyCat951@users.noreply.github.com>
2022-08-25 18:12:56 +0100

    Added the run server file
    This file will run the server when the file is ran!

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo PLEASE RUN THIS SCRIPT AS SUDO!
+echo ---------------------------------------------------------------------------
+echo IF YOU DID NOT RUN THIS SCRIPT WITH SUDO PRESS CTRL + C TO EXIT THE SCRIPT!
+echo ---------------------------------------------------------------------------
+echo please wait...
+sleep 10
+echo please enter the path to the server.jar file DO NOT PUT THE JAR FILE IN THE PATH! e.g /minecraft-server
+read path
+echo $path is the path you ented!
+sleep 10
+echo please enter the amount of ram the minecraft server can use. e.g. 512M or 1G
+read mem
+echo you choose $mem
+sleep 10
+echo ------
+echo PAPER!
+echo ------
+cd $path && java -Xmx$mem -jar paper.jar
