6445e41eca247389384600e29b0ff4b35cca5fc1
Ghost_cat174 <93802888+Ghost-Cat174@users.noreply.github.com>
2022-08-26 09:58:47 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ echo PLEASE RUN THIS SCRIPT AS SUDO!
 echo ---------------------------------------------------------------------------
 echo IF YOU DID NOT RUN THIS SCRIPT WITH SUDO PRESS CTRL + C TO EXIT THE SCRIPT!
 echo ---------------------------------------------------------------------------
+echo YOU CAN MOVE THIS FILE TO WHERE YOU WANT BECUCE IT ASKS FOR THE PATH OF THE paper.jar FILE
 echo please wait...
 sleep 10
 echo please enter the path to the server.jar file DO NOT PUT THE JAR FILE IN THE PATH! e.g /minecraft-server
